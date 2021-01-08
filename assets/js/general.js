1ce2e1844ecc65ceb517ecae109538ae803f7743
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-01-08 12:56:48 +0300

    Add click handler to the burger in the nav

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,5 +1,8 @@
 document.addEventListener( 'DOMContentLoaded', () => {
 
+  /**
+   * Настройка ScrollFectJS
+   */
   ScrollFect.appearance( 'nav.top-nav', {
     animation: ScrollFect.animationStore.appearanceSlideTop,
     duration: 0.75,
@@ -17,6 +20,17 @@ document.addEventListener( 'DOMContentLoaded', () => {
     params: {
       minScale: 0.95
     }
+  } );
+
+
+  /**
+   * Навигационная панель на мобильных устройствах
+   */
+  document.querySelector( '.top-nav__burger' ).addEventListener( 'click', function () {
+    let buttons = document.querySelector( '.top-nav__buttons' );
+    buttons.classList.contains( 'shown' )
+      ? ( buttons.classList.remove( 'shown' ), document.body.style.overflow = 'auto' )
+      : ( buttons.classList.add( 'shown' ), document.body.style.overflow = 'hidden' );
   } );
 
 } );
